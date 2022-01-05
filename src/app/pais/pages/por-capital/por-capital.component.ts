bd2cfc8127a481352d76a7bcad4d401d4cd846d0
camilo <camilojuang11@gmail..com>
2022-01-05 09:34:30 -0500

    PaisesApp

---
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-capital.component.html',
+  styles: [
+  ]
+})
+export class PorCapitalComponent implements OnInit {
+  termino:string=""
+  hayError:boolean=false
+  paises:Country[]=[];
+  constructor(private paisService:PaisService) { }
+
+  ngOnInit(): void {
+  }
+  buscar(termino:string){
+    this.hayError=false
+    this.termino=termino
+    this.paisService.buscarCapital(termino)
+      .subscribe((paises)=>{
+        console.log(paises)
+        this.paises=paises;
+      },(err)=>{
+        this.hayError=true
+        this.paises=[]
+      });
+      
+  }
+  
+}
---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [`
+    li{
+      cursor:pointer
+    }
+  `
+  ]
+})
+export class PorPaisComponent implements OnInit {
+  termino:string=""
+  hayError:boolean=false
+  paises:Country[]=[];
+  paisesSugeridos:Country[]=[];
+
+  constructor(private paisService:PaisService) { }
+
+  ngOnInit(): void {
+  }
+  buscar(termino:string){
+    this.hayError=false
+    this.termino=termino
+    this.paisService.buscarPais(termino)
+      .subscribe((paises)=>{
+        console.log(paises)
+        this.paises=paises;
+      },(err)=>{
+        this.hayError=true
+        this.paises=[]
+      });
+      
+  }
+  sugerencias(termino:string){
+    this.hayError=false
+    this.paisService.buscarPais(termino)
+      .subscribe(paises=>this.paisesSugeridos=paises.splice(0,5),
+      err=>this.paisesSugeridos=[])
+  }
+}
